b3f7d7988b62374e628b05ee5471e6dd1157a629
OdilovMM <dssrinman@gmail.com>
2024-01-06 22:10:10 +0900

    Add: complated User routes with JWT authorization and user statistics

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const cryptojs = require("crypto-js");
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
 // REGISTER
 
@@ -34,12 +35,22 @@ router.post("/login", async (req, res) => {
       process.env.PASSWORD_SECRET
     );
     const originalPassword = hashedPassword.toString(cryptojs.enc.Utf8);
-    
-    originalPassword !== req.body.password && res.status(401).json("Wrong credentials");
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" }
+    );
 
     const { password, ...others } = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({...others, accessToken});
   } catch (err) {
     res.status(500).json(err);
   }
