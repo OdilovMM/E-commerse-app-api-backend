20fc9f0606c56f9bc55bf72a5d753d7114e29168
OdilovMM <dssrinman@gmail.com>
2024-01-07 02:57:55 +0900

    Add: added stripe route

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ const authRoute = require("./routes/auth.js");
 const productRoute = require("./routes/product.js");
 const cartRoute = require("./routes/cart.js");
 const orderRoute = require("./routes/order.js");
+const stripeRoute = require('./routes/stripe.js')
+const cors = require('cors')
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection is successfull"))
   .catch((err) => console.log(err));
 
+  app.use(cors())
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use('/api/checkout', stripeRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port 5000");
